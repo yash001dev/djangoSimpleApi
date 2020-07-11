508c9ceeb722b8afee9b41716cdbe6561a77891d
yash001dev <y.sanghavi001@gmail.com>
2020-07-11 14:43:13 +0530

    first commit

---
+++ profiles_api/views.py
@@ -0,0 +1,46 @@
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+
+from profiles_api import serializers
+
+class HelloApiView(APIView):
+    # Test API View
+
+    serializer_class=serializers.HelloSerializer
+
+    def get(self,request,format=None):
+        # Returns a list of APIView Features
+        an_apiview=[
+            'Uses HTTP methods as function(get,post,patch,put,delete)',
+            'Is similar to a traditional Django View',
+            'Gives you the most control over you application logic',
+            'Is mapped manually to URLs',
+             
+        ]
+        return Response({'message':'Hello!','an_apiview':an_apiview})
+
+    def post(self,request):
+        # Create a Hello Message With Our Name
+        serializer=self.serializer_class(data=request.data)
+
+        if serializer.is_valid():
+            name=serializer.validated_data.get('name')
+            message= f'Hello{name}'
+            return Response({'message':message})
+
+        else:
+            return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
+
+        
+    def put(self,request,pk=None):
+        # Handle Update Object
+        return Response({'method':'PUT'})
+
+    def patch(self,request,pk=None):
+        # Handle A Partial Update of an Object
+        return Response({'method':'PATCH'})
+
+    def delete(self,request,pk=None):
+        #Delete an Object
+        return Response({'method':'DELETE'})
