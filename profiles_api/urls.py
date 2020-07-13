ae5198acae3af2e519e8e8f08b5b06c1b084f66d
yash001dev <y.sanghavi001@gmail.com>
2020-07-13 15:46:21 +0530

    Complete CRUD Operation With Viewsets

--- profiles_api/urls.py
+++ profiles_api/urls.py
@@ -3,9 +3,9 @@ from rest_framework.routers import DefaultRouter
 from profiles_api import views
 
 router=DefaultRouter()
-router.register('hello-viewset',views.HelloViewSet,base_name='hello-viewset')
+router.register('hello-viewset',views.HelloViewSet,basename='hello-viewset')
 
 urlpatterns = [
-    path('hello-view/',views.HelloApiView.as_view()),
+    path('helloview/',views.HelloApiView.as_view()),
     path('',include(router.urls))
 ]
